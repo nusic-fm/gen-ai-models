844ecfad40cd26a829e30a94e4cd758c151de17c
zeeshanhanif <zeeshanhanif@gmail.com>
2024-06-18 23:48:02 +0500

    Added script to update number of tracks generated

---
+++ scripts/05_gen-ai-update-tracks.ts
@@ -0,0 +1,29 @@
+import { ethers, network } from "hardhat";
+import { GenAIModel2, GenAIModel2__factory } from "../typechain-types";
+const addresses = require("./address.json");
+
+async function main() {
+
+  const [owner, addr1] = await ethers.getSigners();
+  console.log("Network = ",network.name);
+
+  const GenAIModel2:GenAIModel2__factory = await ethers.getContractFactory("GenAIModel2");
+  const genAIModel2:GenAIModel2 = await GenAIModel2.attach(addresses[network.name].genAIModel2);
+  await genAIModel2.deployed();
+
+  console.log("GenAIModel2 Address to:", genAIModel2.address);
+
+  const txt = await genAIModel2.updateNoOfTracksGenerated("user1",5);
+  console.log("txt.hash = ",txt.hash);
+  const receipt = await txt.wait();
+  console.log("receipt = ",receipt);
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
